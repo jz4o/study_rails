4f4484cf9adf6ab7c64573da845d31eaf7992566
jz4o <26317360+jz4o@users.noreply.github.com>
2018-06-25 23:29:49 +0900

    ファイルアップロード機能から不要な記述を削除

--- app/controllers/feature_carrier_wave/users_controller.rb
+++ app/controllers/feature_carrier_wave/users_controller.rb
@@ -1,23 +1,17 @@
 class FeatureCarrierWave::UsersController < ApplicationController
   before_action :set_feature_carrier_wave_user, only: [:show, :edit, :update, :destroy]
 
-  # GET /feature_carrier_wave/users
-  # GET /feature_carrier_wave/users.json
   def index
     @feature_carrier_wave_users = FeatureCarrierWave::User.all
   end
 
-  # GET /feature_carrier_wave/users/1
-  # GET /feature_carrier_wave/users/1.json
   def show
   end
 
-  # GET /feature_carrier_wave/users/new
   def new
     @feature_carrier_wave_user = FeatureCarrierWave::User.new
   end
 
-  # GET /feature_carrier_wave/users/1/edit
   def edit
   end
 
@@ -26,46 +20,29 @@ class FeatureCarrierWave::UsersController < ApplicationController
     @feature_carrier_wave_user.avatar.cache! if feature_carrier_wave_user_params[:avatar]
   end
 
-  # POST /feature_carrier_wave/users
-  # POST /feature_carrier_wave/users.json
   def create
     @feature_carrier_wave_user = FeatureCarrierWave::User.new(feature_carrier_wave_user_params)
 
     @feature_carrier_wave_user.avatar.retrieve_from_cache! params[:cache][:avatar] if params.dig(:cache,:avatar)&.present?
 
-    respond_to do |format|
-      if @feature_carrier_wave_user.save
-        format.html { redirect_to @feature_carrier_wave_user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @feature_carrier_wave_user }
-      else
-        format.html { render :new }
-        format.json { render json: @feature_carrier_wave_user.errors, status: :unprocessable_entity }
-      end
+    if @feature_carrier_wave_user.save
+      redirect_to @feature_carrier_wave_user, notice: 'User was successfully created.'
+    else
+      render :new
     end
   end
 
-  # PATCH/PUT /feature_carrier_wave/users/1
-  # PATCH/PUT /feature_carrier_wave/users/1.json
   def update
-    respond_to do |format|
-      if @feature_carrier_wave_user.update(feature_carrier_wave_user_params)
-        format.html { redirect_to @feature_carrier_wave_user, notice: 'User was successfully updated.' }
-        format.json { render :show, status: :ok, location: @feature_carrier_wave_user }
-      else
-        format.html { render :edit }
-        format.json { render json: @feature_carrier_wave_user.errors, status: :unprocessable_entity }
-      end
+    if @feature_carrier_wave_user.update(feature_carrier_wave_user_params)
+      redirect_to @feature_carrier_wave_user, notice: 'User was successfully updated.'
+    else
+      render :edit
     end
   end
 
-  # DELETE /feature_carrier_wave/users/1
-  # DELETE /feature_carrier_wave/users/1.json
   def destroy
     @feature_carrier_wave_user.destroy
-    respond_to do |format|
-      format.html { redirect_to feature_carrier_wave_users_url, notice: 'User was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to feature_carrier_wave_users_url, notice: 'User was successfully destroyed.'
   end
 
   private
--- app/views/feature_carrier_wave/users/index.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.array! @feature_carrier_wave_users, partial: 'feature_carrier_wave_users/feature_carrier_wave_user', as: :feature_carrier_wave_user
