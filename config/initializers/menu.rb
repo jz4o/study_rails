b2f0592c7348cd5aa76a2b4d578d3cdf2329d1b1
jz4o <26317360+jz4o@users.noreply.github.com>
2018-07-24 23:56:26 +0900

    メニューの内容を拡張

--- config/initializers/menu.rb
+++ config/initializers/menu.rb
@@ -8,9 +8,21 @@ module Menu
   MenuItem = Struct.new :title, :description, :demo_path, :codes, keyword_init: true
 
   @items << MenuItem.new(
+    title:       'メニュー画面',
+    demo_path:   '/menu',
+    codes:       Dir.glob('**/*menu*/**/*.*').sort
+  )
+
+  @items << MenuItem.new(
     title:       '関連テーブルを含む登録更新処理',
     description: 'accept_nested_forを用いて、関連テーブルのレコードも併せて更新するサンプルコード',
     demo_path:   '/feature_active_record/parents',
     codes:       Dir.glob('**/*feature_active_record*/**/*.*').sort
+  )
+
+  @items << MenuItem.new(
+    title:       'CarrierWaveを用いたファイルアップロード',
+    demo_path:   '/feature_carrier_wave/users',
+    codes:       Dir.glob('**/*feature_carrier_wave*/**/*.{coffee,scss,rb,erb}').sort
   )
 end
