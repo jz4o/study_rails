c729fd46d33c99626066976f1444ac2874491fee
jz4o <26317360+jz4o@users.noreply.github.com>
2018-07-26 00:31:51 +0900

    メニューをリファクタリング

--- config/initializers/menu.rb
+++ config/initializers/menu.rb
@@ -5,24 +5,32 @@ module Menu
     @items
   end
 
+  def self.item_file_paths(feature_name, extensions = %w[cofee scss rb erb])
+    [
+      *Dir.glob("**/*#{feature_name}*/**/*.{#{extensions.join(',')}}"),
+      *Dir.glob("**/*#{feature_name}*.{#{extensions.join(',')}}")
+    ].uniq.sort
+  end
+  private_class_method :item_file_paths
+
   MenuItem = Struct.new :title, :description, :demo_path, :codes, keyword_init: true
 
   @items << MenuItem.new(
     title:       'メニュー画面',
     demo_path:   '/menu',
-    codes:       Dir.glob('**/*menu*/**/*.*').sort
+    codes:       item_file_paths('menu')
   )
 
   @items << MenuItem.new(
     title:       '関連テーブルを含む登録更新処理',
     description: 'accept_nested_forを用いて、関連テーブルのレコードも併せて更新するサンプルコード',
     demo_path:   '/feature_active_record/parents',
-    codes:       Dir.glob('**/*feature_active_record*/**/*.*').sort
+    codes:       item_file_paths('feature_active_record')
   )
 
   @items << MenuItem.new(
     title:       'CarrierWaveを用いたファイルアップロード',
     demo_path:   '/feature_carrier_wave/users',
-    codes:       Dir.glob('**/*feature_carrier_wave*/**/*.{coffee,scss,rb,erb}').sort
+    codes:       item_file_paths('feature_carrier_wave')
   )
 end
